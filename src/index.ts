f410d405bb591e40b4178b310ca3a2d265f25a8c
kelsny <cursors.owo.uwu@gmail.com>
2022-10-15 23:38:35 -0500

    static iter fns

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export { iter } from "./iter/index.js";
+import {
+    empty,
+    from_fn,
+    from_generator,
+    iter,
+    once,
+    once_with,
+    repeat,
+    repeat_with,
+    successors,
+    zip,
+} from "./iter/index.js";
+
+const __iter__ = Object.assign(iter, {
+    empty,
+    from_fn,
+    from_generator,
+    once,
+    once_with,
+    repeat,
+    repeat_with,
+    successors,
+    zip,
+});
+
+export { __iter__ as iter };
--- src/iter/index.ts
+++ src/iter/index.ts
@@ -7,3 +7,49 @@ export const empty = () => iter();
 
 //TODO: need Option<T> type
 export const from_fn = <T, R>(f: () => IteratorResult<T, R>) => toIter({ next: f });
+
+export const from_generator = <T, R, N>(generator: Generator<T, R, N>) => toIter(generator);
+
+export const once = <T>(value: T) => toIter([value]);
+
+export const once_with = <T>(gen: () => T) =>
+    toIter(
+        (function* () {
+            yield gen.call(undefined);
+        })(),
+    );
+
+export const repeat = <T>(elt: T) =>
+    toIter(
+        (function* () {
+            while (true) yield structuredClone(elt);
+        })(),
+    );
+
+export const repeat_with = <T>(repeater: () => T) =>
+    toIter(
+        (function* () {
+            while (true) yield repeater.call(undefined);
+        })(),
+    );
+
+//TODO: need Option<T> type
+export const successors = <T>(first: T, succ: (item: T) => T) =>
+    toIter(
+        (function* () {
+            //
+        })(),
+    );
+
+export const zip = <A, B>(a: IterResolvable<A>, b: IterResolvable<B>) => toIter(a).zip(b);
+
+console.log(
+    repeat_with(() => empty())
+        .take(5)
+        .collect(),
+);
+console.log(
+    Array(5)
+        .fill(0)
+        .map(() => empty()),
+);
