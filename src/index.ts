d302be73e754962a368ff0222aa4f25f4aadafed
kelsny <cursors.owo.uwu@gmail.com>
2022-10-19 17:18:39 -0500

    tested option methods

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const __iter__ = Object.assign(iter, {
     zip,
 });
 
-// MUST TEST ALL OPTION METHODS
+// MUST TEST ALL RESULT METHODS
 
 export { __iter__ as iter };
 export { __option__ as option };
--- src/option/Option.ts
+++ src/option/Option.ts
@@ -1,7 +1,7 @@
 import deepEqual from "deep-equal";
 import { toIter } from "../iter/helpers/toIter.js";
 import type { Iter } from "../iter/Iter.js";
-import { Ok, Result } from "../result/Result.js";
+import { Err, Ok, Result } from "../result/Result.js";
 import { Namespaced } from "../utils/namespaced.js";
 import { unenumerable } from "../utils/unenumerable.js";
 
@@ -19,7 +19,7 @@ class _<T> {
             this.#value = args[0];
         }
 
-        unenumerable(this as _<T>, ["is_some_and", "unwrap_or", "unwrap_or_else"]);
+        unenumerable(this as _<T>, ["filter", "is_some_and"]);
     }
 
     and<U>(optb: Option<U>): Option<U> {
@@ -42,6 +42,7 @@ class _<T> {
         return this.cloned();
     }
 
+    contains(value: [T] extends [never] ? unknown : T): [T] extends [never] ? false : boolean;
     contains(value: T): boolean {
         return this.#none ? false : deepEqual(this.#value, value, { strict: true });
     }
@@ -52,13 +53,14 @@ class _<T> {
         return this.#value;
     }
 
-    filter<U extends T = T>(predicate: (value: T) => value is U): Option<U>;
-    filter<_>(predicate: (value: T) => boolean): Option<T>;
-    filter(predicate: (value: T) => boolean): Option<T> {
+    filter: [T] extends [never] ? (f: (item: never) => boolean) => None : filter<T> = function filter(
+        this: _<T>,
+        predicate: (value: T) => boolean,
+    ): Option<T> {
         if (this.#none) return None;
 
         return predicate.call(undefined, this.#value) ? new _(this.#value) : None;
-    }
+    } as _<T>["filter"];
 
     flatten(): T extends Option<infer U> ? Option<U> : never;
     flatten() {
@@ -69,7 +71,8 @@ class _<T> {
         throw new TypeError(`cannot flatten a value that is not an option`);
     }
 
-    get_or_insert(value: T): T {
+    get_or_insert(value: [T] extends [never] ? unknown : T): T;
+    get_or_insert(value: T) {
         if (this.#none) {
             this.#none = false;
             this.#value = value;
@@ -78,10 +81,12 @@ class _<T> {
         return this.#value;
     }
 
+    get_or_insert_with(f: () => [T] extends [never] ? unknown : T): T;
     get_or_insert_with(f: () => T): T {
         return this.get_or_insert(f.call(undefined));
     }
 
+    insert(value: [T] extends [never] ? unknown : T): T;
     insert(value: T): T {
         this.#none = false;
         this.#value = value;
@@ -134,22 +139,35 @@ class _<T> {
         return f.call(undefined, this.#value);
     }
 
-    ok_or<E>(err: E) {}
+    ok_or<E>(err: E): T extends T ? Result<T, E> : never;
+    ok_or<E>(err: E): Result<T, E> {
+        if (this.#none) return Err(err);
 
-    ok_or_else<E>(err: () => E) {}
+        return Ok(this.#value);
+    }
 
+    ok_or_else<E>(err: () => E): T extends T ? Result<T, E> : never;
+    ok_or_else<E>(err: () => E): Result<T, E> {
+        if (this.#none) return Err(err.call(undefined));
+
+        return Ok(this.#value);
+    }
+
+    or(optb: [T] extends [never] ? unknown : Option<T>): Option<T>;
     or(optb: Option<T>): Option<T> {
         if (!this.#none) return new _(this.#value);
 
         return optb.#none ? None : new _(optb.#value);
     }
 
+    or_else(f: () => [T] extends [never] ? unknown : Option<T>): Option<T>;
     or_else(f: () => Option<T>): Option<T> {
         if (!this.#none) return new _(this.#value);
 
         return f.call(undefined);
     }
 
+    replace(value: [T] extends [never] ? unknown : T): Option<T>;
     replace(value: T): Option<T> {
         const none = this.#none;
         const original = this.#value;
@@ -172,9 +190,11 @@ class _<T> {
 
     transpose(): T extends Result<infer O, infer E> ? Result<Option<O>, E> : never;
     transpose() {
-        if (this.#none) return Ok(None);
+        if (this.#none) return Ok(None) as Result<any, any>;
 
-        return this.#value as Result<any, any>;
+        const result = this.#value as Result<any, any>;
+
+        return result.is_err() ? Err(result.unwrap_err()) : Ok(Some(result.unwrap()));
     }
 
     unwrap(): T {
@@ -183,27 +203,27 @@ class _<T> {
         return this.#value;
     }
 
-    unwrap_or: [T] extends [never] ? (fallback: unknown) => never : (fallback: T) => T = function unwrap_or(
-        this: _<T>,
-        fallback: T,
-    ): T {
+    unwrap_or(fallback: [T] extends [never] ? unknown : T): T;
+    unwrap_or(fallback: T): T {
         if (this.#none) return fallback;
 
         return this.#value;
-    } as _<T>["unwrap_or"];
+    }
 
-    unwrap_or_else: [T] extends [never] ? (fallback: () => unknown) => never : (fallback: () => T) => T =
-        function unwrap_or_else(this: _<T>, fallback: () => T) {
-            if (this.#none) return fallback.call(undefined);
+    unwrap_or_else(fallback: () => [T] extends [never] ? unknown : T): T;
+    unwrap_or_else(fallback: () => T): T {
+        if (this.#none) return fallback.call(undefined);
 
-            return this.#value;
-        } as _<T>["unwrap_or_else"];
+        return this.#value;
+    }
 
     unwrap_unchecked(): T {
         return this.#value;
     }
 
-    unzip(): [T] extends [[infer A, infer B]] ? [Some<A>, Some<B>] | [None, None] : never;
+    // need testing:
+
+    unzip(): [T] extends [readonly [infer A, infer B]] ? [Some<A>, Some<B>] | [None, None] : never;
     unzip() {
         if (this.#none) return [None, None];
 
@@ -212,6 +232,7 @@ class _<T> {
         return [new _(a), new _(b)];
     }
 
+    xor<U>(optb: [T] extends [never] ? Option<U> : Option<T>): Option<[T] extends [never] ? U : T>;
     xor(optb: Option<T>): Option<T> {
         if (this.#none && !optb.#none) return new _(optb.#value);
         if (!this.#none && optb.#none) return new _(this.#value);
@@ -219,6 +240,7 @@ class _<T> {
         return None;
     }
 
+    zip<U>(other: Option<U>): T extends T ? Option<[T, U]> : never;
     zip<U>(other: Option<U>): Option<[T, U]> {
         if (!this.#none && !other.#none) return new _([this.#value, other.#value]);
 
@@ -246,6 +268,11 @@ class _<T> {
 export interface is_some_and<T> {
     <U extends T = T>(f: (item: T) => item is U): this is Is & Some<U>;
     <_>(f: (item: T) => boolean): this is Is & Some<T>;
+}
+
+export interface filter<T> {
+    <U extends T = T>(predicate: (value: T) => value is U): Option<U>;
+    <_>(predicate: (value: T) => boolean): Option<T>;
 }
 
 const narrowed = Symbol();
